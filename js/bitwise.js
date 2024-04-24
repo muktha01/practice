36d3d14692ed046371094e05fbde67dc4afa2f47
muktha01 <venkatb12127@gmail.com>
2024-04-24 12:51:37 +0530

    new file added

---
+++ js/bitwise.js
@@ -0,0 +1,9 @@
+let email = prompt("enter your email ");
+let pass = prompt("enter your password ")
+
+
+if(email== "mukthanandha01@gmail.com "  && pass=="venky123") {
+    console.log("email login is successful.....")
+}else{
+    console.log("email/password is invalid...")
+}
---
+++ js/first.js
@@ -0,0 +1,19 @@
+let email = "mukthanandha01@gmail.com";
+let pass = "venky123";
+
+
+if(email=="mukthanandha01@gmail.com" && pass=="venky123" ){
+    console.log("email login is successful")
+
+}else{
+    console.log("pass/email is invalid")
+}
+
+
+
+
+const colors = ['red', 'green', 'blue'];
+
+for (let i = 0; i == colors.length; i++) {
+  console.log(colors[i]);
+}
